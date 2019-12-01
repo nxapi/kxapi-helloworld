df717dbe3c70e45c1e1705ab4c4fe6106d26e4c2
pengxiang <923164250@qq.com>
2019-12-01 19:07:20 +0800

    feat: 调试

--- src/controllers/dto/test-dto.ts
+++ src/controllers/dto/test-dto.ts
@@ -3,5 +3,7 @@ import { d } from '@nxapi/nxapi';
 export default class TestDto {
   @d.number.max(1)
   @d.number.description('ddd')
+  @d.number.required()
+  @d.number.error(() => '自定义错误')
   tt: number;
 }
--- src/controllers/test-controller.ts
+++ src/controllers/test-controller.ts
@@ -2,13 +2,14 @@ import TestReq from './req/test-req';
 import TestDto from './dto/test-dto';
 import { d } from '@nxapi/nxapi';
 import { BaseController } from '@nxapi/kxapi';
+import BaseResponse from './dto/base-response';
 @d.controller.path('/v1')
 export default class TestController extends BaseController {
   @d.function.description('ddddddd')
   @d.function.get('/hello')
-  public ggg(req: TestReq): number[] {
+  public ggg(req: TestReq): BaseResponse<TestDto> {
     this.service.test3Service.test();
-    return [3];
+    return null;
   }
   @d.function.post('post')
   public hhh(req: TestReq): TestDto {
